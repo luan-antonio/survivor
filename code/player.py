51ddd8f6b29fabeaf4f4e37c26e6e568511ead34
Luan Antonio <46446054+luan-antonio@users.noreply.github.com>
2024-12-14 18:33:29 -0300

    feat(app): Add player walk animation

--- code/player.py
+++ code/player.py
@@ -4,6 +4,8 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
+        self.load_animation_frames()
+        self.state, self.frame_index = 'down', 0
         self.image = pygame.image.load(
             join('images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_frect(center=pos)
@@ -13,6 +15,22 @@ class Player(pygame.sprite.Sprite):
         self.direction = pygame.Vector2(0, 0)
         self.speed = 500
         self.collision_sprites = collision_sprites
+
+    def load_animation_frames(self):
+        self.frames = {
+            'left': [],
+            'right': [],
+            'up': [],
+            'down': [],
+        }
+
+        for state in self.frames.keys():
+            for folder_path, sub_folders, file_names in walk(join('images', 'player', state)):
+                if file_names:
+                    for file_name in sorted(file_names, key=lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -41,6 +59,19 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y > 0:
                         self.hitbox_rect.bottom = sprite.rect.top
 
+    def animate(self, dt):
+        # get state
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+        if self.direction.y != 0:
+            self.state = 'down' if self.direction.y > 0 else 'up'
+
+        # animate
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(
+            self.frame_index) % len(self.frames[self.state])]
+
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
